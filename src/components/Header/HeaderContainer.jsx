d318d81a3398de65129406c958e2623c09191d3b
Jenya <113167825+Genya10@users.noreply.github.com>
2023-08-04 11:55:31 +0300

    add thunk auth

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -2,29 +2,20 @@ import { connect } from "react-redux";
 import React from "react";
 import Header from "./Header";
 import axios from "axios";
-import { setAuthUserData } from "../../state/auth-reducer";
-import { auth } from "../../state/auth-reducer";
 import { authAPI } from "../../api/api";
+import { getAuthUserData } from "../../state/auth-reducer";
 
 class HeaderContainer extends React.Component{
     componentDidMount(){
-authAPI.me()
+        this.props.getAuthUserData();
+
+/*authAPI.me()
 .then((response)=>{
     if(response.data.resultCode===0){
         let {id,login,email}=response.data.data;
         this.props.setAuthUserData(id,email,login)
     }
-});
-       /* axios
-        .get('https://social-network.samuraijs.com/api/1.0/auth/me',{
-            withCredentials:true
-        })
-        .then((response)=>{
-            if(response.data.resultCode===0){
-                let {id,login,email}=response.data.data;
-                this.props.setAuthUserData(id,email,login)
-            }
-        });*/
+});*/
     }
     render (){
         return <Header {...this.props}/>
@@ -38,5 +29,5 @@ const mapStateToProps=(state)=>({
 
 
 
-export default connect(mapStateToProps,{setAuthUserData})(HeaderContainer);
+export default connect(mapStateToProps,{getAuthUserData})(HeaderContainer);
 
