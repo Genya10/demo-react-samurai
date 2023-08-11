fd340146c834e3afe2a296988beb4a088db9e22d
Jenya <113167825+Genya10@users.noreply.github.com>
2023-08-11 18:22:55 +0300

    add validator login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { Input } from "../common/Preloader/FormsControls/FormsControls";
+import { required,maxLengthCreator } from "../../utils/validators/validator";
 
+const maxLength40= maxLengthCreator(40);
 
+/*
 const LoginForm=(props)=>{
     return (    
        <form onSubmit={props.handleSubmit}>
@@ -11,7 +15,22 @@ const LoginForm=(props)=>{
    <div><button>Login</button></div>
    </form>   
     )
-    }
+    }*/
+
+    const LoginForm=(props)=>{
+        return (    
+           <form onSubmit={props.handleSubmit}>
+       <div><Field placeholder={"Login"} name={'login'} component={Input}
+       validate={[required,maxLength40]}/></div> 
+       <div><Field placeholder={"Password"} name={'password'} component={Input}
+       validate={[required,maxLength40]}/></div>
+       <div><Field type="checkbox" name={'remember me'}  component={Input}
+       validate={[required]} />remember me</div>
+       <div><button>Login</button></div>
+       </form>   
+        )
+        }
+    
 
 const LoginReduxForm = reduxForm({form:'login'})(LoginForm)
 
--- src/components/common/Preloader/FormsControls/FormsControls.jsx
+++ src/components/common/Preloader/FormsControls/FormsControls.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import cl from "./FormsControls.module.css"
+ 
 
 export const Textarea=({input,meta,...props})=>{
-
-    const hasError=meta.touched && meta.error;
-    
+    const hasError=meta.touched && meta.error;  
 return(
     <div >
         <div >
@@ -15,3 +14,38 @@ return(
     </div>
 )
 }
+
+export const Input=({input,meta,...props})=>{
+    const hasError=meta.touched && meta.error;  
+return(
+    <div >
+        <div >
+        <input {...input}{...props}
+        className={cl.formControl + " "+ (hasError ? cl.error:"")} {...props.input}/>
+        </div>
+       {hasError && <span>{meta.error}</span>}
+    </div>
+)
+}
+
+/*
+const FormControl =(input,meta,children,...props)=>{
+    const hasError=meta.touched && meta.error;  
+    return(
+        <div >
+            <div className={cl.formControl + " "+ (hasError ? cl.error:"")}>
+           {props.children}
+            </div>
+           {hasError && <span>{meta.error}</span>}
+        </div>
+    )
+}
+
+export const Textarea=(props)=>{
+    const {input,meta,child,...restProps}=props;
+return <FormControl {...props}><textarea {...input}{...restProps}/></FormControl>
+}
+export const Input=(props)=>{
+    const {input,meta,child,...restProps}=props;
+return <FormControl {...props}><input {...input}{...restProps}/></FormControl>
+}*/
