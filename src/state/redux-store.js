a733cae9a9108b6a7cd93b35f169da23b8f07713
Jenya <113167825+Genya10@users.noreply.github.com>
2023-08-21 10:07:29 +0300

    add extend for redux

--- src/state/redux-store.js
+++ src/state/redux-store.js
@@ -7,6 +7,7 @@ import authReducer from "./auth-reducer";
 import ThunkMiddleware from "redux-thunk";
 import {reducer as formReducer } from "redux-form";
 import appReducer from "./app-reducer";
+import { compose } from "redux";
 
 let reduces = combineReducers({
     profilePage:profileReducer,
@@ -18,7 +19,10 @@ let reduces = combineReducers({
     app:appReducer
 });
 
-export let store = createStore(reduces,applyMiddleware(ThunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = createStore(reduces, composeEnhancers(applyMiddleware(ThunkMiddleware)));
+
+//export let store = createStore(reduces,applyMiddleware(ThunkMiddleware));
 
 
 
